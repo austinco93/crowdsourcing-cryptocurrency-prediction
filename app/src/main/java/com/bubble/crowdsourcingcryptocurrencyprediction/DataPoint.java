c06ffb21d82d0d11196bd0d324d0cfdad5cf88b6
Ted Weber <webert3@wwu.edu>
2018-02-10 15:44:07 -0800

    Datapoint class.

---
+++ app/src/main/java/com/bubble/crowdsourcingcryptocurrencyprediction/DataPoint.java
@@ -0,0 +1,15 @@
+package com.bubble.crowdsourcingcryptocurrencyprediction;
+
+/**
+ * Created by webert3 on 2/10/18.
+ */
+
+public class DataPoint {
+    int x;
+    int y;
+
+    public DataPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
