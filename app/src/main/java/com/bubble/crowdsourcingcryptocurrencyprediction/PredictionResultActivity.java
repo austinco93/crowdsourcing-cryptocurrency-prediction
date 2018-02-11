d5ee98a2fa591d4f85778fa013dcaabbc1ed1da8
tonmoy2114 <tonmoy2114@gmail.com>
2018-02-11 02:30:49 -0800

    bar chart added

--- app/src/main/java/com/bubble/crowdsourcingcryptocurrencyprediction/PredictionResultActivity.java
+++ app/src/main/java/com/bubble/crowdsourcingcryptocurrencyprediction/PredictionResultActivity.java
@@ -1,5 +1,6 @@
 package com.bubble.crowdsourcingcryptocurrencyprediction;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,19 @@ import android.widget.TextView;
 
 import com.bubble.crowdsourcingcryptocurrencyprediction.model.RetrievedPredictionEntry;
 import com.bubble.crowdsourcingcryptocurrencyprediction.model.UserPredictionEntry;
+import com.github.mikephil.charting.charts.BarChart;
+
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,6 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -40,6 +55,41 @@ public class PredictionResultActivity extends AppCompatActivity {
         getAllData();
     }
 
+    public void setUpChart(String[] values, ArrayList<BarEntry> yval) {
+        HorizontalBarChart chart = (HorizontalBarChart) findViewById(R.id.chart);
+
+        BarDataSet set1;
+        set1 = new BarDataSet(yval, "Overall Prediction");
+
+        set1.setColors(Color.parseColor("#F78B5D"), Color.parseColor("#FCB232"), Color.parseColor("#FDD930"), Color.parseColor("#ADD137"), Color.parseColor("#A0C25A"));
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(dataSets);
+
+        // hide Y-axis
+        YAxis left = chart.getAxisLeft();
+        left.setDrawLabels(false);
+
+        // custom X-axis labels
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new MyXAxisValueFormatter(values));
+
+        chart.setData(data);
+
+        // custom description
+        Description description = new Description();
+        description.setText("Rating");
+        chart.setDescription(description);
+
+        // hide legend
+        chart.getLegend().setEnabled(false);
+
+        chart.animateY(1000);
+        chart.invalidate();
+    }
+
     private void getAllData() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -49,20 +99,33 @@ public class PredictionResultActivity extends AppCompatActivity {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         HashMap<String, Integer> data = new HashMap<String, Integer>();
-                        for (DataSnapshot dsInner : dataSnapshot.getChildren()) {
-                            data.put(dsInner.getKey(), (int) dsInner.getChildrenCount());
-                        }
+                        ArrayList<String> xAxis = new ArrayList<>();
+                        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
                         int i = 0;
+                        for (DataSnapshot dsInner : dataSnapshot.getChildren()) {
+                            xAxis.add(dsInner.getKey());
+                            BarEntry v1e1 = new BarEntry(i, (int) dsInner.getChildrenCount());
+                            valueSet1.add(v1e1);
+                            data.put(dsInner.getKey(), (int) dsInner.getChildrenCount());
+                            i++;
+                        }
+                        i = 0;
                         for (Map.Entry entry : data.entrySet()) {
                             allTextView[i].setText(entry.getKey() + ": " + entry.getValue() + "");
                             allTextView[i].setVisibility(View.VISIBLE);
                             i++;
+                        }
+                        String[] xData = new String[i];
+                        for (int j = 0; j < i; j++) {
+                            xData[j] = xAxis.get(j);
                         }
                         textViewTotalSubmitted.setText("Total people perticipated in the prediction: " + i);
 //                        tvOption1.setText(data.get(0)+"");
 //                        tvOption2.setText(data.get(0)+"");
 //                        tvOption3.setText(data.get(0)+"");
 //                        tvOption4.setText(data.get(0)+"");
+
+                        setUpChart(xData, valueSet1);
 
 
                     }
@@ -72,5 +135,21 @@ public class PredictionResultActivity extends AppCompatActivity {
                         //handle databaseError
                     }
                 });
+    }
+
+
+    public class MyXAxisValueFormatter implements IAxisValueFormatter {
+
+        private String[] mValues;
+
+        public MyXAxisValueFormatter(String[] values) {
+            this.mValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return mValues[(int) value];
+        }
+
     }
 }
